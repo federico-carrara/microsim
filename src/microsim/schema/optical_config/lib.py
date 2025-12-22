a3bc035ab04290529f6c79e7b36bbd9c04c726a0
federico-carrara <federico1.carrara@mail.polimi.it>
2025-12-22 14:45:11 +0100

    feat: max range of considered wvs is from the minimum excitation laser -50 until the right extremum of emission wv range +50

--- src/microsim/schema/optical_config/lib.py
+++ src/microsim/schema/optical_config/lib.py
@@ -135,7 +135,7 @@ def spectral_detector(
     lights = [
         LightSource.laser(laser, power) for laser, power in zip(lasers, laser_powers)
     ]
-    waves = np.arange(min_wave - 100, max_wave + 100, 1)
+    waves = np.arange(min(lasers) - 50, max_wave + 50, 1)
 
     # create fake bandpass ... could also be something like 80/20
     laser0, *rest = lasers
